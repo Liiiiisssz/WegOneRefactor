5d4e27dd82707f877c59de126cb4713e5299c90d
Lis <elis_jasper@estudante.sesisenai.org.br>
2026-02-16 13:55:24 -0300

    feat/finaliza menu

--- CodigoRefatorado/src/main/java/br/com/senai/centroweg/wegone/view/Menu.java
+++ CodigoRefatorado/src/main/java/br/com/senai/centroweg/wegone/view/Menu.java
@@ -2,9 +2,14 @@ package br.com.senai.centroweg.wegone.view;
 
 import br.com.senai.centroweg.wegone.util.Sc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Menu {
     public void exibirMenu(){
-        System.out.println("""
+        Map<Integer, Runnable> opcoes = new HashMap<>();
+        while(true){
+            System.out.println("""
                  ______________________________
                 | 1.  Cadastrar Orientação     |
                 | 2.  Pesquisar Orientação     |
@@ -14,16 +19,22 @@ public class Menu {
                 | 5. X ENCERRAR PROGRAMA X     |
                 |______________________________|
                 """);
-        int opcao = Sc.lerInt();
-        switch (opcao){
-            case 1 -> controller.cadastrar();
-            case 2 -> controller.pesquisar();
-            case 3 -> controller.editar();
-            case 4 -> controller.excluir();
-            case 5 -> System.out.println("Encerrando sistema...");
-            default -> {
-                System.out.println("Número inválido!");
+            int opcao = Sc.lerInt();
+
+            opcoes.put(1, controller::cadastro);
+            opcoes.put(2, controller::pesquisa);
+            opcoes.put(3, controller::editar);
+            opcoes.put(4, controller::excluir);
+            opcoes.put(5, () -> {
+                System.out.println("Encerrando...");
                 System.exit(0);
+            });
+
+            Runnable acao = opcoes.get(opcao);
+            if (acao != null) {
+                acao.run();
+            } else {
+                System.out.println("Número inválido!");
             }
         }
     }
