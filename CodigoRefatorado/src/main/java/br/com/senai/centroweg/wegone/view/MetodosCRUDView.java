422ca32c54f5debc48dedd9c422c4f8fcae3b089
KAEL LUIH DE ARAUJO <kael_araujo@estudante.sesisenai.org.br>
2026-02-19 14:47:58 -0300

    Fix: Corrigi erros de arquitetura que ferem o padrao SRP

---
+++ CodigoRefatorado/src/main/java/br/com/senai/centroweg/wegone/view/MetodosCRUDView.java
@@ -0,0 +1,17 @@
+package br.com.senai.centroweg.wegone.view;
+
+public interface MetodosCRUDView  {
+
+
+
+    void cadastrar();
+    void todas();
+    void pesquisar();
+    void editar();
+    void excluir();
+
+
+
+
+
+}
